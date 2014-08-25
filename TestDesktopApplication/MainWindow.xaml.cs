13b4f544338dee5e3d8f7f7f015cf6489846732d
mubtxs <mubtxs.android@gmail.com>
2014-08-25 20:37:07 +0300

    rtyyyerge eyteyyteygeg

--- TestDesktopApplication/MainWindow.xaml.cs
+++ TestDesktopApplication/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace TestDesktopApplication
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
     {
         public MainWindow()
         {
